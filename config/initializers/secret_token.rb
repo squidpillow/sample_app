fbaa3756ff27efbcccff2e220ca6db8106d11d56
Foo Bar <foobar@Super-Air.local>
2012-05-02 09:02:04 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5605dfb4d363c31cb999c2607565001182274f4f989fe470c1d789b889742e96e84ac12b71593e05a6b1f8b0434c09273164fd7e268604d3cb9366d563abc548'
