5325e4d468909eb414eb709b003197890fa40ca2
Foo Bar <foobar@Super-Air.local>
2012-05-06 09:40:17 -0400

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+	
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
@@ -34,4 +34,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  # Speed up tests by lowering BCrypt's cost function
+	require 'bcrypt'
+	silence_warnings do
+		BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+	end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name	"Bluto Eye Boogers"
+		email "bluto@buttgrass.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
