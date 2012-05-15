5ca0ed9852cf1266983ee937fd5656e04108099c
Foo Bar <foobar@Super-Air.local>
2012-05-15 10:39:58 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_filter :correct_user, only: [:edit, :update]
+	before_filter :admin_user, only: :destroy
+	
+	def edit
+	end
 	
 	def show
 		@user = User.find(params[:id])
@@ -18,5 +24,44 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+  	if @user.update_attributes(params[:user])
+  		flash[:success] = "Profile update"
+  		sign_in @user
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  end
+  
+  def index
+  	@users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "User destroyed."
+  	redirect_to users_path
+  end
+  
+  private
+  	
+  	def signed_in_user
+  		unless signed_in?
+  			store_location
+  			redirect_to signin_path, notice: "Please sign in."
+  		end
+  	end
+  	
+  	def correct_user
+  		@user = User.find(params[:id])
+  		redirect_to(root_path) unless current_user?(@user)
+  	end
+  	
+  	def admin_user
+  		redirect_to(root_path) unless current_user.admin?
+  	end
+  	
 end
 
